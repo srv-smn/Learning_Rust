bdcc01c1a7cde49cfa4cde467a17f77165aa6219
sourav suman <souravsuman@souravs-MacBook-Air.local>
2023-01-19 08:34:22 +0530

    if statement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+fn if_statement() {
+    let temp =  25;
+    if temp > 30 {
+        println!("reaally hot outside");
+    } else if  temp < 10{
+        println!("really cold")
+    }else {
+        println!("temperature is ok")
+    }
+    let day = if temp > 20 {"sunny"} else {"cloudy"};
+    println!("today is {}", day);
+
+    println!("it is {}", if temp > 20 {"hot"} else if temp < 10 {"cold"} else {"OK"});
+
+}
 fn main() {
-    println!("Hello World");
+    if_statement();
 }
