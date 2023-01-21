90d096f4c096d60e8677ffb27f87ddfeafe996d3
sourav suman <souravsuman@souravs-MacBook-Air.local>
2023-01-21 15:57:47 +0530

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+struct Point {
+    x: f64,
+    y: f64
+}
+struct Line {
+    start: Point,
+    end: Point
+}
+
+fn structures() {
+    let p1 = Point{x: 3.0, y: 4.0};
+    let p2 = Point{x:5.0, y: 10.0};
+
+    let myLine = Line{start: p1, end: p2};
+    println!("line start at ({},{}) and ends at ({},{})", myLine.start.x,myLine.start.y, myLine.end.x, myLine.end.y);
+}
 fn main() {
-    println!("Hello World");
+    //struct
+    structures();
 }
