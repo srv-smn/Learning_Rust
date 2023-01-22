0acf1c8e901fdde1e7afcb778b8a8e3414a74615
sourav suman <souravsuman@souravs-MacBook-Air.local>
2023-01-22 17:12:01 +0530

    union

--- src/main.rs
+++ src/main.rs
@@ -46,10 +46,46 @@ fn enums(){
         _ => ()
     }
 }
+
+// union
+/*
+Union is a concept taken from c/c++ which can contain value 
+of different type, but only any one of its property can have
+value.
+since union can have value for any of the property, so it is 
+not sure which data we will get while reading 
+so this read are unsafe
+ */
+union IntOrFloat {
+    i:i32,
+    f:f32
+}
+
+fn process_value(iof: IntOrFloat){
+    unsafe{
+        match iof {
+            IntOrFloat {i:42 } => {println!("meaning of life");}
+            IntOrFloat{f} => {println!("f32 = {}",f);}
+        }
+    }
+}
+
+fn unions(){
+    let mut iof = IntOrFloat{i:123};
+     iof.i = 42;
+    let value = unsafe {iof.i};
+    process_value(iof);
+    process_value(IntOrFloat{f: 1.23});
+
+
+}
 fn main() {
     //struct
    // structures();
 
    //enum
-   enums();
+   //enums();
+
+   //union
+   unions();
 }
