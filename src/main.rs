8aff995f1b92fe3ba1f1b06b6169c0a9186ada7a
sourav suman <souravsuman@souravs-MacBook-Air.local>
2023-02-08 17:37:53 +0530

    pattern matching

--- src/main.rs
+++ src/main.rs
@@ -310,6 +310,38 @@ fn tuples()
 
     }
 
+    fn how_many(x: i32) -> &'static str
+    {
+        match x
+            {
+                0 => "no",
+                1|2 => "one or two",
+                12 => "a dozen",
+                z @ 9..=11 => "lots of",
+                _ if (x%2 ==0) => "some",
+                _ => "a few"
+            }
+    }
+
+    fn pattern_matching()
+        {
+            for x in 0..13
+                {
+                    println!("{}: I have {} oranges", x, how_many(x));
+                }
+            let point = (0,7);
+
+            match point
+            {
+                (0,0) => println!("origin"),
+                (0,y) => println!("x axis, y = {}", y),
+                (x,0) => println!("y axis, x = {}",x) ,
+                // if we want to paas the argument as reference and modifiy
+                // (ref mut x,0) => println!("y axis, x = {}",x) ,
+                (_, y) => println!("(?, {})", y) 
+            }
+        }
+
 
 fn main() {
     //struct
@@ -337,5 +369,8 @@ fn main() {
   //strings();
 
   // tuples
-  tuples();
+  //tuples();
+
+  // pattern matching
+  pattern_matching();
 }
