4b7f4238f993b8659ee215ce427444591d997093
sourav suman <souravsuman@souravs-MacBook-Air.local>
2023-02-13 15:07:20 +0530

    closure

--- src/main.rs
+++ src/main.rs
@@ -59,8 +59,55 @@ fn methods()
     let myLine = Line{start:p1, end: p2};
     println!("length = {}", myLine.len());
 }
+
+fn say_hello() { println!("hello");}
+
+fn closures()
+{   
+    // we can assign a variable 
+    // this concepts will be used for closure
+    let sh = say_hello;
+    sh();
+
+    // declaring function and assign it to variables
+    // this function will have a limited scopes
+
+    let plus_one = |x:i32| -> i32 {x+1};
+    let a =6;
+
+    println!("{}+1 = {}",a, plus_one(a));
+
+    let mut two = 2;
+    { // everything declared under this scope will get destroyed 
+        // after the closing of the bracket, this is done to avoid 
+        // borrow issue
+        let plus_two = |x|
+        {
+            let mut z =x;
+            z +=two;
+            z 
+        };
+        println!("{} + 2 = {}",3,plus_two(3));
+    }
+    let borrow_two = &mut two;
+    /**
+     * ways to call a function
+     * T: by value
+     * T& by reference
+     * &mut by mut reference
+    */
+
+    let plus_three = |x: &mut i32| *x += 3;
+
+    let mut f = 12;
+    plus_three(&mut f);
+    println!("f = {}",f);
+}
 fn main() {
     //functions();
 
-    methods();
+   // methods();
+
+    // closure
+    closures();
 }
